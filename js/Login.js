a61407d89f7684e9a385003add3a3c2177f3c249
0mnr0 <savelijln0@gmail.com>
2024-04-05 10:50:33 +0300

    -

--- js/Login.js
+++ js/Login.js
@@ -16,6 +16,17 @@ function LoginScript(){
 		'password': Pass,
 		'username': LoginVal}
 
-	window.frames[0].frameElement.contentWindow.GetBearer(Pass, LoginVal).then(res => {console.log(res)})
-
+	//window.frames[0].frameElement.contentWindow.GetBearer(Pass, LoginVal).then(res => {console.log(res)})
+	axios.post('https://msapi.top-academy.ru/api/v2/auth/login', {
+		username: 'ilinx_uk78',
+		application_key: '6a56a5df2667e65aab73ce76d1dd737f7d1faef9c52e8b8c55ac75f565d8e8a6',
+		id_city: null,
+		password: 'DontStealMyHW0000'
+	  })
+	  .then(function (response) {
+		console.log(response);
+	  })
+	  .catch(function (error) {
+		console.log(error);
+	  });
 }

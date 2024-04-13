495bd11f6466afab6ee1907a91b1bc5b0b610da2
0mnr0 <savelijln0@gmail.com>
2024-04-13 11:37:45 +0400

    Clear Debug infos

--- JournalUIProxy.py
+++ JournalUIProxy.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import json
-import logging
-import http.client as http_client
-
 
 d = 'https://msapi.top-academy.ru/api/v2/auth/login'
 
@@ -18,7 +15,6 @@ def process_request():
     fetchType = data.get('reqMethod')
 
     url = data.get('fetchUrl')
-    print('Bearer: ', (data.get('Bearer')))
     headers = {
         'Referer': 'https://journal.top-academy.ru/',
         'Origin': 'https://journal.top-academy.ru',
@@ -32,24 +28,12 @@ def process_request():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
     }
 
-    
-
-    #print("data['includeJson']: ", (type(json.loads(data['includeJson']))), data['includeJson'])
-
     js = {
       "application_key": "6a56a5df2667e65aab73ce76d1dd737f7d1faef9c52e8b8c55ac75f565d8e8a6",
       "id_city": None,
       "password": include.get('password'),
       "username": include.get('username')
     }
-
-    http_client.HTTPConnection.debuglevel = 1
-    # You must initialize logging, otherwise you'll not see debug output.
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
 
     totalCommandExecute = 'requests.'+(fetchType.lower())+'(url, headers=headers'
     if (len(include)) == 0:
